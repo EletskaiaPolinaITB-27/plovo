acd1b3ec30fca794cca1831f238252a64adfd915
Student <student@compass.local>
2026-03-17 12:40:44 +0600

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -3,17 +3,22 @@ import { Header } from "./components/Header/Header"
 import { Home } from "./Pages/Home/Home"
 import { Dish } from "./Pages/Dish/Dish"
 import { AddDish } from "./Pages/Add-dish/AddDish"
+import { Container } from "@mui/material"
 
 function App() {
   
   return (
     <>
       <Header/>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="/dish/:id" element={<Dish/>}/>
-        <Route path="/dish/create" element={<AddDish/>}/>
+      <Container style={{
+        padding:'20px'
+      }}>
+        <Routes>
+          <Route path="/" element={<Home/>}/>
+          <Route path="/dish/:id" element={<Dish/>}/>
+          <Route path="/dish/create" element={<AddDish/>}/>
       </Routes>
+      </Container>
     </>
   )
 }
